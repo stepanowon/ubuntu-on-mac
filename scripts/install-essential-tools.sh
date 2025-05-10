9058b30f952bc522d7041977c3821b9addb8a2e5
stepanowon <stepanowon@hotmail.com>
2025-05-10 14:55:02 +0900

    chagen box

--- scripts/install-essential-tools.sh
+++ scripts/install-essential-tools.sh
@@ -5,7 +5,8 @@ set -euo pipefail
 #sed -i 's/archive.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list.d/ubuntu.sources
 #sed -i 's/security.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list.d/ubuntu.sources
 
-y#apt upgrade -y
+apt update 
+#apt upgrade -y
 DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
 
 apt install virtualbox-guest-additions-iso -y
